8a8f0e1d233e995d0f3d95fded41bdcb706c7f16
Ahmed Negm <160288929+x7007x@users.noreply.github.com>
2025-05-24 07:23:42 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="TeleFlow",
-    version="0.1.1",
+    version="0.2.0",
     author="Ahmed Negm",
     author_email="A7medNegm.x@gmail.com",
     description="Asynchronous Telegram Bot API client",
